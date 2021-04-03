8a0d44df821646e9bf93f5a4e3a15295004b666f
Junior <juniorpeter159@gmail.com>
2021-04-03 04:29:51 +0100

    minor update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,13 +4,13 @@
                     <div class="container-fluid">
                         <div class="nk-content-inner">
                             <div class="nk-content-body">
-                                <div class="nk-block-head nk-block-head-sm">
+                                <!--<div class="nk-block-head nk-block-head-sm">
                                     <div class="nk-block-between">
                                         <div class="nk-block-head-content">
                                             <h4 class="nk-block-title page-title">Dashboard</h4>
-                                        </div><!-- .nk-block-head-content -->
-                                    </div><!-- .nk-block-between -->
-                                </div><!-- .nk-block-head -->
+                                        </div>
+                                    </div>
+                                </div> -->
                                 <div class="nk-block">
                                     <div class="row mb-5">
                                         <div class="col-12">

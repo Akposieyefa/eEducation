8acb3324f3e55229b3dc0973757b841124dc2636
Junior <juniorpeter159@gmail.com>
2021-04-20 13:40:37 +0100

    result sheet update

--- app/Imports/SubjectResultSheet.php
+++ app/Imports/SubjectResultSheet.php
@@ -32,24 +32,22 @@ class SubjectResultSheet  implements ToModel, WithStartRow
     {
         $id = 0;
         // TODO: Implement model() method.
-        $resultCheck = Result::where(['subject_id' => $this->subject_id,'term_id' => $this->term_id,'level_id' => $this->level_id])->get();
-          if ($resultCheck) {
-            dd('fgdfhdfhfdh');
+        $resultCheck = Result::where(['subject_id' => $this->subject_id, 'term_id' => $this->term_id, 'level_id' => $this->level_id])->get();
+        if (count($resultCheck) > 0) {
+            //dd('fgdfhdfhfdh');
             DB::table('results')->where('student_id', $row['0'])->update([
-                 'ca_score'    =>  $row[4],
-                 'exam_score'    =>  $row[5],
-             ]);
-
-          }else {
-               return new Result([
-                   'student_id'     => $row[0],
-                   'ca_score'    =>  $row[4],
-                   'exam_score'    =>  $row[5],
-                   'term_id' => $this->term_id,
-                   'subject_id' => $this->subject_id,
-                   'level_id' => $this->level_id
-               ]);
-          }
-
+                'ca_score'    =>  $row[4],
+                'exam_score'    =>  $row[5],
+            ]);
+        } else {
+            return new Result([
+                'student_id'     => $row[0],
+                'ca_score'    =>  $row[4],
+                'exam_score'    =>  $row[5],
+                'term_id' => $this->term_id,
+                'subject_id' => $this->subject_id,
+                'level_id' => $this->level_id
+            ]);
+        }
     }
 }
